3c54f8c02429cb41d61477f17ac3f2d43ec22c03
GreatDain <52960331+GreatDain@users.noreply.github.com>
2022-03-30 17:10:58 +0200

    Camera transition and animation work.
    Animations trigger and cameras switch for beginning of standoff.

--- Assets/Scripts/FaceOffScripts/Duel_Manager.cs
+++ Assets/Scripts/FaceOffScripts/Duel_Manager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Duel_Manager : MonoBehaviour
 {
     public List<GameObject> Dueling = new List<GameObject>();
     public List<Transform> visible = new List<Transform>();
-    public Transform playerTrans;
 
-    public float walkSpeed = 1f;
-    private int Limiter = 0;
+    public List<CinemachineVirtualCamera> duelCamera = new List<CinemachineVirtualCamera>();
+    public GameObject playerTrans;
+    public GameObject target;
+    private float walkSpeed = 1f;
+    public int Limiter = 0;
     public Letterbox letterBox;
+    public float transitionTimer = 2f;
     
     // Start is called before the first frame update
     void Start()
@@ -21,23 +25,51 @@ public class Duel_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && Limiter == 0){
-            // FindObjectOfType<Player_Duel_Behaviour>().enabled = false;
+        if (Input.GetKeyDown(KeyCode.Space) && Limiter == 0 && visible.Count >= 0){
+            FindObjectOfType<Player_Duel_Behaviour>().enabled = false;
             FindDuelists();
             letterBox.Show(150f, 0.3f);
-            playerTrans.LookAt(Dueling[0].transform);
-            float step = walkSpeed * Time.deltaTime;
-            playerTrans.position = Vector3.MoveTowards(playerTrans.position, Dueling[0].transform.position, step);
             Limiter = 1;
+            CameraControl();
+            // StartCoroutine("CinematicWalkTimer");
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && Limiter == 1){
-            // FindObjectOfType<Player_Duel_Behaviour>().enabled = true;
+        if (Input.GetKeyDown(KeyCode.LeftShift)){
+            FindObjectOfType<Player_Duel_Behaviour>().enabled = true;
             letterBox.Hide(0.3f);
             Dueling.Clear();
             Limiter = 0;
+            CameraControl();
+        }
+
+        if (Limiter == 1){
+            float step = walkSpeed * Time.deltaTime;
+            playerTrans.transform.position = Vector3.MoveTowards(playerTrans.transform.position, Dueling[0].transform.position, step);
+            playerTrans.GetComponent<Animator>().SetFloat("Blend", walkSpeed, 0.3f, Time.deltaTime);
+            foreach (GameObject obj in Dueling){
+                obj.transform.position = Vector3.MoveTowards(obj.transform.position, playerTrans.transform.position, step);
+                obj.GetComponent<Animator>().SetFloat("Blend", 0.3f, 0.3f, Time.deltaTime);
+            }
+            
+            // print("Moving");
         }
     }
+
+    void CameraControl(){
+        if (Limiter == 1){
+            FindObjectOfType<CinemachineFreeLook>().enabled = false;
+            duelCamera[0].gameObject.SetActive(true);
+        }else if (Limiter == 0){
+            FindObjectOfType<CinemachineFreeLook>().enabled = true;
+            duelCamera[0].gameObject.SetActive(false);
+        }
+    }
+
+    // IEnumerator CameraTransition(){
+    //     print("Switch");
+    //     yield return new WaitForSeconds(2f);
+    //     print("Switch");
+    // }
 
     public void FindDuelists(){
         if (visible.Count < 3 && visible.Count > 0 && Dueling.Count < 3){
@@ -59,6 +91,8 @@ public class Duel_Manager : MonoBehaviour
                 col.radius = Mathf.Lerp(0, 20, x);
             }
         }
+
+        playerTrans.transform.LookAt(Dueling[0].transform);
         // PreDuel();
     }
 
