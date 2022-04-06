d1127cbedb4badb232bbcec76a1108fff39f8e09
GreatDain <52960331+GreatDain@users.noreply.github.com>
2022-04-06 21:22:13 +0200

    Screen fade in and out and enemy position control for duel phases

--- Assets/Scripts/FaceOffScripts/Duel_Manager.cs
+++ Assets/Scripts/FaceOffScripts/Duel_Manager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class Duel_Manager : MonoBehaviour
 {
     public List<GameObject> Dueling = new List<GameObject>();
     public List<Transform> visible = new List<Transform>();
+    public List<Transform> duelpositions = new List<Transform>();
 
     public List<CinemachineVirtualCamera> duelCamera = new List<CinemachineVirtualCamera>();
     public GameObject playerTrans;
@@ -16,16 +18,35 @@ public class Duel_Manager : MonoBehaviour
     private int transition = 0;
     public Letterbox letterBox;
     public float transitionTimer = 2f;
+    private bool pause;
+
+    public GameObject blackSquare;
     
     // Start is called before the first frame update
     void Start()
     {
         visible = FindObjectOfType<FieldOfView>().visibleTargets;
+        pause = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            StartCoroutine(FadeToBlack());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)){
+            StartCoroutine(FadeBack());
+        }
+
+        if (pause == false && Input.GetKeyDown(KeyCode.P)){
+            Time.timeScale = 0;
+            pause = true;
+        }else if (pause == true && Input.GetKeyDown(KeyCode.P)){
+            Time.timeScale = 1;
+            pause = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && Limiter == 0 && visible.Count >= 0){
             FindObjectOfType<Player_Duel_Behaviour>().enabled = false;
             FindDuelists();
@@ -45,15 +66,33 @@ public class Duel_Manager : MonoBehaviour
 
         if (Limiter == 1){
             float step = walkSpeed * Time.deltaTime;
-            playerTrans.transform.position = Vector3.MoveTowards(playerTrans.transform.position, Dueling[0].transform.position, step);
+            playerTrans.transform.LookAt(Dueling[0].transform);
+            // playerTrans.transform.position = Vector3.MoveTowards(playerTrans.transform.position, Dueling[0].transform.position, step);
             playerTrans.GetComponent<Animator>().SetFloat("Blend", walkSpeed, 0.3f, Time.deltaTime);
             foreach (GameObject obj in Dueling){
                 // obj.transform.position = Vector3.MoveTowards(obj.transform.position, playerTrans.transform.position, step);
                 obj.GetComponent<Animator>().SetFloat("Blend", 0.3f, 0.3f, Time.deltaTime);
+                obj.GetComponent<Animator>().SetBool("Draw", false);
+                obj.GetComponent<Animator>().SetBool("Attack", false);
             }
-            
+
+            if (transition <= 1){
+                playerTrans.transform.position = Vector3.MoveTowards(playerTrans.transform.position, Dueling[0].transform.position, step);
+                foreach (GameObject obj in Dueling){
+                    obj.transform.position = Vector3.MoveTowards(obj.transform.position, playerTrans.transform.position, step);
+                }
+            }     
+
+            if (transition == 2){
+                print(Dueling[0].GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime);
+                if (Dueling[0].GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.4f && Input.GetKeyDown(KeyCode.G)){
+                    playerTrans.GetComponent<Animator>().SetBool("Idle->Atk1", true);
+                    Dueling[0].GetComponent<Animator>().SetBool("Death", true);
+                }
+            } 
             // print("Moving");
         }
+        
     }
 
     void CameraSwitch(){
@@ -76,18 +115,42 @@ public class Duel_Manager : MonoBehaviour
     }
 
     IEnumerator CameraTransition(){
-        yield return new WaitForSeconds(2f);
         if (transition == 0){
+            yield return new WaitForSeconds(3f);
+            StartCoroutine(FadeToBlack());
+            yield return new WaitForSeconds(0.5f);
+            // yield return new WaitForSeconds(1f);
             duelCamera[0].gameObject.SetActive(false);
-            duelCamera[1].gameObject.SetActive(true);
+            for (int x = 0; x < Dueling.Count; x++){
+                Dueling[x].transform.position = duelpositions[x].position;
+            }
             transition = 1;
-            StartCoroutine(CameraTransition());
+            duelCamera[1].gameObject.SetActive(true);
+            // StartCoroutine(CameraTransition());
             print("Switch 1");
+            // StartCoroutine(FadeBack());
         }else if (transition == 1){
+            yield return new WaitForSeconds(1f);
+            StartCoroutine(FadeToBlack());
+            yield return new WaitForSeconds(0.5f);
             duelCamera[1].gameObject.SetActive(false);
+            for (int x = 0; x < Dueling.Count; x++){
+                Dueling[x].transform.position = duelpositions[x+3].position;
+                Dueling[x].GetComponent<Animator>().SetBool("Draw", true);
+            }
+            playerTrans.GetComponent<Animator>().SetBool("Walk->Idle", true);
+            transition = 2;
             duelCamera[2].gameObject.SetActive(true);
+            // StartCoroutine(CameraTransition());
             print("Switch 2");
+            // StartCoroutine(FadeBack());
+        }else if (transition == 2){
+            yield return new WaitForSeconds(Random.Range(2f, 6f));
+            Dueling[0].GetComponent<Animator>().SetBool("Attack", true);
         }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(FadeBack());
+        // StartCoroutine(CameraTransition());
     }
 
     public void FindDuelists(){
@@ -112,12 +175,37 @@ public class Duel_Manager : MonoBehaviour
         }
 
         playerTrans.transform.LookAt(Dueling[0].transform);
-        // PreDuel();
     }
 
-    // public void PreDuel(){
+    IEnumerator FadeToBlack(float fadeSpeed = 3.5f){
+        Color objectColour = blackSquare.GetComponent<Image>().color;
+        float fadeAmount;
+        while(blackSquare.GetComponent<Image>().color.a < 1){
+            fadeAmount = objectColour.a + (fadeSpeed * Time.deltaTime);
 
-    // }
+            objectColour = new Color(objectColour.r, objectColour.g, objectColour.b, fadeAmount);
+            blackSquare.GetComponent<Image>().color = objectColour;
+            yield return null;
+        }
+        // yield return new WaitForSeconds(0.5f);
+        // StartCoroutine(FadeBack());
+    }
+    
+    IEnumerator FadeBack(float fadeSpeed = 3.5f){
+        Color objectColour = blackSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        while(blackSquare.GetComponent<Image>().color.a > 0){
+            fadeAmount = objectColour.a - (fadeSpeed * Time.deltaTime);
+
+            objectColour = new Color(objectColour.r, objectColour.g, objectColour.b, fadeAmount);
+            blackSquare.GetComponent<Image>().color = objectColour;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(CameraTransition());
+    }
+
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Enemy" && Dueling.Count < 3){
--- Assets/Scripts/FaceOffScripts/Player_Duel_Behaviour.cs
+++ Assets/Scripts/FaceOffScripts/Player_Duel_Behaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(CharacterController))]
 public class Player_Duel_Behaviour : MonoBehaviour
@@ -32,6 +33,7 @@ public class Player_Duel_Behaviour : MonoBehaviour
 
     public float verticalVel;
     private Vector3 moveVector;
+
     // public Letterbox letterBox;
 
     // Start is called before the first frame update
@@ -45,6 +47,10 @@ public class Player_Duel_Behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene("Standoff");
+        }
+
         InputMagnitude ();
 
         isGrounded = controller.isGrounded;
